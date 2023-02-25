84cad35858d0e4665cd00a202d7e15b25e087fec
lawtonwillingham <lwillingh@gmail.com>
2023-02-25 10:58:38 -0500

    Figuring it out

--- primes/src/lib.rs
+++ primes/src/lib.rs
@@ -7,85 +7,88 @@
 /// # Examples
 /// 
 /// ```
-/// use primes::nth_prime;
+/// use primes::primes::nth_prime;
 /// let result = nth_prime(4);
 /// assert_eq!(result, 7);
 /// ```
-pub fn nth_prime(n: u64) -> u64 {
-    if n == 1 {
-        return 2;
-    };
-    if n == 2 {
-        return 3;
-    };
+/// 
+pub mod primes{
+    pub fn nth_prime(n: u64) -> u64 {
+        if n == 1 {
+            return 2;
+        };
+        if n == 2 {
+            return 3;
+        };
 
-    let mut count = 2;
-    let mut test = 3;
+        let mut count = 2;
+        let mut test = 3;
 
-    while count < n {
-        test += 2;
-        if is_prime(test) {
-            count += 1;
+        while count < n {
+            test += 2;
+            if is_prime(test) {
+                count += 1;
+            }
         }
+
+        test
     }
 
-    test
-}
+    pub fn is_prime(n: u64) -> bool {
+        if n == 2 || n == 3 {
+            return true;
+        };
 
-pub fn is_prime(n: u64) -> bool {
-    if n == 2 || n == 3 {
-        return true;
-    };
+        let mut divisor = 2;
 
-    let mut divisor = 2;
-
-    if n % divisor == 0 {
-        return false;
-    };
-
-    divisor += 1;
-
-    let limit: u64 = ((n as f64).sqrt()) as u64 + 1;
-
-    while divisor < limit {
         if n % divisor == 0 {
             return false;
         };
-        divisor += 2;
+
+        divisor += 1;
+
+        let limit: u64 = ((n as f64).sqrt()) as u64 + 1;
+
+        while divisor < limit {
+            if n % divisor == 0 {
+                return false;
+            };
+            divisor += 2;
+        }
+
+        true
     }
 
-    true
-}
+    #[cfg(test)]
+    mod tests {
+        use super::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        #[test]
+        fn test_nth_prime() {
+            assert_eq!(nth_prime(1), 2);
+            assert_eq!(nth_prime(2), 3);
+            assert_eq!(nth_prime(3), 5);
+            assert_eq!(nth_prime(4), 7);
+            assert_eq!(nth_prime(5), 11);
+            assert_eq!(nth_prime(347), 2341);
+        }
 
-    #[test]
-    fn test_nth_prime() {
-        assert_eq!(nth_prime(1), 2);
-        assert_eq!(nth_prime(2), 3);
-        assert_eq!(nth_prime(3), 5);
-        assert_eq!(nth_prime(4), 7);
-        assert_eq!(nth_prime(5), 11);
-        assert_eq!(nth_prime(347), 2341);
-    }
+        #[test]
+        fn is_prime_with_primes() {
+            assert_eq!(is_prime(2), true);
+            assert_eq!(is_prime(3), true);
+            assert_eq!(is_prime(5), true);
+            assert_eq!(is_prime(7), true);
+            assert_eq!(is_prime(11), true);
+        }
 
-    #[test]
-    fn is_prime_with_primes() {
-        assert_eq!(is_prime(2), true);
-        assert_eq!(is_prime(3), true);
-        assert_eq!(is_prime(5), true);
-        assert_eq!(is_prime(7), true);
-        assert_eq!(is_prime(11), true);
-    }
-
-    #[test]
-    fn is_prime_with_non_primes() {
-        assert_eq!(is_prime(4), false);
-        assert_eq!(is_prime(6), false);
-        assert_eq!(is_prime(8), false);
-        assert_eq!(is_prime(9), false);
-        assert_eq!(is_prime(12), false);
+        #[test]
+        fn is_prime_with_non_primes() {
+            assert_eq!(is_prime(4), false);
+            assert_eq!(is_prime(6), false);
+            assert_eq!(is_prime(8), false);
+            assert_eq!(is_prime(9), false);
+            assert_eq!(is_prime(12), false);
+        }
     }
 }
--- project_euler/src/main.rs
+++ project_euler/src/main.rs
@@ -1,6 +1,6 @@
 mod euler001;
+mod euler002;
 use crate::euler001::euler001::solution;
-
 
 fn main() {
     println!("Hello, world!");
