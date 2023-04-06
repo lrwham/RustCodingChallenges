42b813508cdaf1f3be5560eb28f61d42ba91a8d5
Lawton Willingham <35542810+lrwham@users.noreply.github.com>
2023-04-06 16:56:40 -0400

    Update main.rs

--- interview_questions/src/main.rs
+++ interview_questions/src/main.rs
@@ -1,9 +1,13 @@
 mod first_occurence_k_times;
 mod sub_array_sum;
 mod helpers;
+mod two_sum;
+mod leetcode;
+
+use crate::leetcode::length_of_longest_substring;
 
 fn main() {
     println!("Hello, world!");
-    first_occurence_k_times::first_occurence_k_times::solution();
-    sub_array_sum::sub_array_sum::solution();
+    crate::leetcode::add_two_numbers::test(9,9999999991);
+
 }
