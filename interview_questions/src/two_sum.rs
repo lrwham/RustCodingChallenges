3aba4eecdffc8f963b0a0c9b9a97230ff7b5aa4b
Lawton Willingham <35542810+lrwham@users.noreply.github.com>
2023-04-06 16:56:42 -0400

    Create two_sum.rs

---
+++ interview_questions/src/two_sum.rs
@@ -0,0 +1,17 @@
+pub mod two_sums{
+    use std::collections::HashMap;
+
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+        
+        for (i, num) in nums.iter().enumerate() {
+            let complement = target - num;
+            if let Some(j) = map.get(&complement) {
+                return vec![*j as i32, i as i32];
+            }
+            map.insert(num, i);
+        }
+        
+        panic!("No solution found");
+    }
+}
