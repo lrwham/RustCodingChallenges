d0008c5115dfbe4a5206cade491fdb610e6f44eb
Lawton Willingham <35542810+lrwham@users.noreply.github.com>
2023-04-07 14:58:11 -0400

    moving modules

---
+++ interview_questions/src/leetcode/two_sum.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map = HashMap::new();
+    
+    for (i, num) in nums.iter().enumerate() {
+        let complement = target - num;
+        if let Some(j) = map.get(&complement) {
+            return vec![*j as i32, i as i32];
+        }
+        map.insert(num, i);
+    }
+    
+    panic!("No solution found");
+}
---
+++ interview_questions/src/other/mod.rs
@@ -0,0 +1,2 @@
+pub mod first_occurence_k_times;
+pub mod sub_array_sum;
