2f9efe834d5a659fa153bdaa77620fe7acb6da52
Lawton Willingham <35542810+lrwham@users.noreply.github.com>
2023-04-07 23:32:06 -0400

    Update main.rs

--- interview_questions/src/main.rs
+++ interview_questions/src/main.rs
@@ -6,7 +6,5 @@ mod other;
 
 fn main() {
     println!("Hello, world!");
-    let a = vec![1,2,3,3,4,7,8,9];
-    let b = vec![];
-    println!("{}", crate::leetcode::find_median_sorted_arrays(a,b));
+    crate::leetcode::zigzag_conversion::convert("hellohello".to_string(), 3);
 }
